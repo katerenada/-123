d2e75ee9c686ddcf4951b21c328cfdfd6c6818df
katerenada <ammarinada060@gmail.com>
2024-12-12 18:39:14 +0100

    Update tp 22.cpp

--- tp 22.cpp
+++ tp 22.cpp
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// تخصيص الذاكرة للسلسلة
 char *ChargerChaine(int N) {
     char *chaine = (char*)malloc(N + 1);  
     if (chaine == NULL) {
         printf("Memory allocation error\n");
-      return 0;
+      return NULL;
     }
     printf("Enter the series: ");
     scanf("%s", chaine);   
@@ -21,13 +22,14 @@ int Longueur(char *ch) {
     return i;
 }
 
+// تحميل السلسلة من المستخدم
 void ChargerTab(char *p, char Tab[]) {
     int i = 0;
     while (p[i] != '\0') {
         Tab[i] = p[i];
         i++;
     }
-    Tab[i] = '\0';  // 
+    Tab[i] = '\0';  // إضافة المحرف '\0' في نهاية السلسلة
 }
 
 void InverserTab(char Tab[], int n) {
@@ -53,25 +55,28 @@ int main() {
 
     if (N <= 0) {
         printf("Please enter a positive number for N.\n");
-        return 1;
+        return 1;  // إذا كانت الذاكرة غير مخصصة بنجاح، التوقف عن التنفيذ
     }
 
     
     char *chaine = ChargerChaine(N);
 
-    
+    // حساب طول السلسلة
     int longueur = Longueur(chaine);
 
+    // تخصيص مصفوفة لتخزين السلسلة
     char Tab[longueur + 1];  
     ChargerTab(chaine, Tab);  
 
+    // طباعة السلسلة الأصلية
     printf("Original series: ");
     AfficherTab(Tab, longueur);
 
+    // عكس السلسلة
     InverserTab(Tab, longueur);
     printf("Inverse series: ");
     AfficherTab(Tab, longueur);
-
+    // تحرير الذاكرة المخصصة
     free(chaine);
 
     return 0;
