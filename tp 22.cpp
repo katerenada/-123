77fac369ee7e264001f799b6f4b5051728e6d568
katerenada <ammarinada060@gmail.com>
2024-12-14 18:56:33 +0100

    Update tp 22.cpp

--- tp 22.cpp
+++ tp 22.cpp
@@ -3,29 +3,29 @@
 
 // تخصيص الذاكرة للسلسلة
 char *ChargerChaine(int N) {
-    char *chaine = (char*)malloc(N + 1);  
+    char *chaine = (char*)malloc(N + 1);   
     if (chaine == NULL) {
-        printf("Memory allocation error\n");
+        printf("Memory allocation error\n");  // إذا فشل تخصيص الذاكرة
       return NULL;
     }
     printf("Enter the series: ");
-    scanf("%s", chaine);   
+    scanf("%s", chaine);   // قراءة السلسلة النصية
 
     return chaine;
 }
 
 int Longueur(char *ch) {
     int i = 0;
-    while (ch[i] != '\0') {
-        i++;
+    while (ch[i] != '\0') { // نمر عبر السلسلة حتى نصل إلى نهاية السلسلة
+        i++;   // زيادة العداد
     }
-    return i;
+    return i;   // إرجاع الطول
 }
 
 // تحميل السلسلة من المستخدم
 void ChargerTab(char *p, char Tab[]) {
     int i = 0;
-    while (p[i] != '\0') {
+    while (p[i] != '\0') {  // ننسخ السلسلة حرفًا بحرف
         Tab[i] = p[i];
         i++;
     }
@@ -35,38 +35,38 @@ void ChargerTab(char *p, char Tab[]) {
 void InverserTab(char Tab[], int n) {
     char temp;
     for (int i = 0; i < n / 2; i++) {
-        temp = Tab[i];
-        Tab[i] = Tab[n - i - 1];
-        Tab[n - i - 1] = temp;
+        temp = Tab[i];   // حفظ العنصر الحالي في مكان مؤقت
+        Tab[i] = Tab[n - i - 1];   // تبديل العنصر الأول بالعنصر الأخير
+        Tab[n - i - 1] = temp;  // وضع العنصر المؤقت في المكان الأخير 
     }
 }
 
 void AfficherTab(char Tab[], int n) {
     for (int i = 0; i < n; i++) {
-        printf("%c", Tab[i]);
+        printf("%c", Tab[i]);   // طباعة كل حرف في السلسلة
     }
-    printf("\n");
+    printf("\n");  // إضافة سطر جديد بعد الطباعة
 }
 
 int main() {
     int N;
     printf("Enter the maximum length of the string: ");
-    scanf("%d", &N);  
+    scanf("%d", &N);    // إدخال طول السلسلة
 
     if (N <= 0) {
-        printf("Please enter a positive number for N.\n");
+        printf("Please enter a positive number for N.\n");  // إذا كان الطول غير صحيح
         return 1;  // إذا كانت الذاكرة غير مخصصة بنجاح، التوقف عن التنفيذ
     }
 
     
-    char *chaine = ChargerChaine(N);
+    char *chaine = ChargerChaine(N);   // قراءة السلسلة
 
     // حساب طول السلسلة
     int longueur = Longueur(chaine);
 
     // تخصيص مصفوفة لتخزين السلسلة
     char Tab[longueur + 1];  
-    ChargerTab(chaine, Tab);  
+    ChargerTab(chaine, Tab);  // نقل السلسلة إلى المصفوفة
 
     // طباعة السلسلة الأصلية
     printf("Original series: ");
